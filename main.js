bea5abc100980e62dd2e4469e818233c0b660dc3
Kreiselinho <dominic.kreisel@student.uibk.ac.at>
2024-05-16 16:35:04 +0200

    Downloadlink eingefügt

--- main.js
+++ main.js
@@ -53,12 +53,17 @@ async function showForecast(url) {
                 </ul>
             `;
             
+            //Wettericons für die nächsten 24 Stunden in 3-Stunden Schritten
             for (let i = 0; i <=24; i += 3) {
                 let symbol = feature.properties.timeseries[i].data.next_1_hours.summary.symbol_code;
                 let time = new Date(feature.properties.timeseries[i].time);
                 content +=`<img src="icons/${symbol}.svg" alt="${symbol}" style="width:32px" title="${time.toLocaleString()}">                `
             }
             
+            //Link zum Datendownload
+            content += `
+                <p><a href="${url}" target="met.no">Daten downloaden </a></p>
+            `;
             L.popup(latlng, {
                 content: content
             }).openOn(themaLayer.forecast);
